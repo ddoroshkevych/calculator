8758fd8efd0d1c616e2c7ca5fb442fec83c455e4
Dmytro Doroshkevych <ddoroshkevych@hotmail.com>
2022-08-07 13:09:21 -0400

    just some example

--- src/App.js
+++ src/App.js
@@ -1,219 +1,55 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [firstOperand, setFirstOperand] = useState("");
-  const [opd1Ready, setOpd1Ready] = useState(false);
-  const [secondOperand, setSecondOperand] = useState("");
-  const [opd2Ready, setOpd2Ready] = useState(false);
+  const [lastOperand, setLastOperand] = useState(0);
+  const [current, setCurrent] = useState("0");
   const [operator, setOperator] = useState(null);
-  const [operatorReady, setOperatorReady] = useState(false);
-  const [answer, setAnswer] = useState(null);
-  const [screen, setScreen] = useState(0);
-  const [tumbler, setTumbler] = useState(true);
-  //const [wholeLine, setWholeLine] = useState("");
-  // const [buffer, setBuffer] = useState(null);
-  const [gotAnswer, setGotAnswer] = useState(false);
+  const [log, setLog] = useState("");
+  const [startNew, setStartNew] = useState(true);
 
-  useEffect(() => {
-    setScreen(firstOperand);
-  }, [firstOperand]);
+  const handleNum = (evt)=>{
+    const value = evt.target.value;
+    setLog(log+value);
+    setCurrent(startNew ? value : current + value );
+    setStartNew(false);
+  }
 
-  useEffect(() => {
-    setScreen(secondOperand);
-  }, [secondOperand]);
+  const calc = ()=>{
+    const res = eval( lastOperand.toString()  + operator + current);
+    setLastOperand(Number.parseFloat(current));
+    setCurrent(res.toString());
+    setStartNew(true);
+  }
 
-  // useEffect(() => {
-  //   setScreen(operator);
-  // }, [operator]);
-
-  useEffect(() => {
-    setScreen(answer);
-  }, [answer]);
-
-  useEffect(() => {
-    setScreen(0);
-  }, []);
-
-  useEffect(() => {
-    setScreen(0);
-  }, [tumbler]);
-
-  function prepareToNextAction() {
-    setFirstOperand("");
-    setSecondOperand("");
+  const handleOp = (evt)=>{
+    setLog(log+evt.target.value);
+    setLastOperand(Number.parseFloat(current));
+    setStartNew(true);
+    setOperator(evt.target.value);
+  }
+  const handleDot = ()=>{
+    setLog(log+".");
+    if(startNew){
+      setCurrent("0.");
+    }else{
+      setCurrent(current+".");
+    }
+  }
+  const processEqual = ()=>{calc();}
+  const resetAll = ()=>{
+    setCurrent("0");
+    setLastOperand(0);
     setOperator(null);
-    //setGotAnswer(true);
-    setOpd1Ready(false);
-    setOpd2Ready(false);
-    setOperatorReady(false);
+    setStartNew(true);
   }
 
-  function handleNum(event) {
-    console.log(
-      "Num  - opd1=",
-      firstOperand,
-      "sign=",
-      operator,
-      "opd2=",
-      secondOperand,
-      "answer=",
-      answer
-    );
-    const value = event.target.value;
-    // setWholeLine((prev) => prev + value);
-
-    //if (!secondOperand && !operator) {
-    if (!opd2Ready && !operatorReady) {
-      setFirstOperand((prevFirstOperand) => prevFirstOperand + value); // 1 opd
-      setOpd1Ready(true);
-      //setWholeLine(prev => prev + value);
-      return;
-    }
-
-    //if (firstOperand && operator) {
-    if (opd1Ready && operatorReady) {
-      setSecondOperand((prevSecondOperand) => prevSecondOperand + value); // 2 opd
-      setOpd2Ready(true);
-      //setWholeLine((prev) => prev + value);
-      return;
-    }
-  }
-
-  function processAnswer(a, b, op) {
-    switch (op) {
-      case "+":
-        return Number(a) + Number(b);
-      case "-":
-        return +a - +b;
-      case "*":
-        return +a * +b;
-      case "/":
-        return +a / +b;
-    }
-  }
-
-  //------------------------------ Handle operator ------------------------------
-
-  function handleOp(event) {
-    console.log(
-      "Oper  - opd1=",
-      firstOperand,
-      "sign=",
-      operator,
-      "opd2=",
-      secondOperand,
-      "answer=",
-      answer
-    );
-    const value = event.target.value;
-    //setWholeLine((prev) => prev + value);
-
-    if (answer && !firstOperand) {
-      // act immediately after answer
-      setFirstOperand(answer);
-      setOpd1Ready(true);
-      setOperator(value);
-      setOperatorReady(true);
-
-      console.log("act immed");
-      return;
-    }
-
-    if (firstOperand) {
-      setOpd1Ready(true); // if op is pressed, opd1 is ready
-    } else return;
-
-    if (opd1Ready && operatorReady && secondOperand) {
-      setSecondOperand("");
-
-      setAnswer(processAnswer(firstOperand, secondOperand, operator));
-
-      setOperator(value);
-      setFirstOperand(processAnswer(firstOperand, secondOperand, operator));
-
-      setOpd1Ready(true);
-      setOpd2Ready(false);
-      setOperatorReady(true);
-
-      return;
-    }
-
-    if (firstOperand && !secondOperand) {
-      setOperator(value);
-      setOperatorReady(true);
-      //setScreen(operator);
-      return;
-    }
-  }
-
-  function processEqual() {
-    if (!opd1Ready || !operatorReady || !secondOperand) return;
-
-    setOpd2Ready(true);
-    switch (operator) {
-      case "+":
-        setAnswer(Number(firstOperand) + Number(secondOperand));
-        prepareToNextAction();
-        return;
-      case "-":
-        setAnswer(+firstOperand - +secondOperand);
-        prepareToNextAction();
-        return;
-      case "*":
-        setAnswer(+firstOperand * +secondOperand);
-        prepareToNextAction();
-        return;
-      case "/":
-        setAnswer(+firstOperand / +secondOperand);
-        prepareToNextAction();
-        return;
-    }
-  }
-
-  function handleDot(event) {
-    const value = event.target.value;
-
-    if (!operatorReady) {
-      if (firstOperand.split("").includes(",") === true) {
-        return;
-      } else {
-        setFirstOperand((prevFirstOperand) => prevFirstOperand + value); // 1 opd
-        return;
-      }
-    } else {
-      if (secondOperand.split("").includes(",") === true) {
-        return;
-      } else {
-        setSecondOperand((prevSecondOperand) => prevSecondOperand + value); // 2 opd
-        return;
-      }
-    }
-  }
-
-  function resetAll() {
-    setFirstOperand("");
-    setSecondOperand("");
-    setOpd1Ready(false);
-    setOpd2Ready(false);
-    setOperator(null);
-    setOperatorReady(false);
-    setAnswer(0);
-    setGotAnswer(false);
-    setScreen(0);
-    setTumbler((prevTumbler) => !prevTumbler);
-  }
+  console.log(current,lastOperand,operator);
 
   return (
     <div className="App">
-      {/* <p>{wholeLine}</p> */}
-      {/* <p>1st opd = {firstOperand}</p>
-      <p>2nd opd = {secondOperand}</p>
-      <p>operation = {operator}</p>
-      <p>answer = {answer}</p> */}
-      <p>{screen}</p>
-
+      <p>{current}</p>
       <button onClick={handleNum} value={"7"}>
         7
       </button>
@@ -267,6 +103,7 @@ function App() {
       </button>
       <br />
       <button onClick={resetAll}>C</button>
+      <p>{log}</p>
     </div>
   );
 }
